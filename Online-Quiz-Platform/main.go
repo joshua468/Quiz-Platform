ade2a6d5cfaa843fae7a732f6afe0d10983a7c68
Adegbesan Joshua Temitope <112911898+joshua468@users.noreply.github.com>
2024-01-30 22:20:03 +0100

    Add files via upload

---
+++ Online-Quiz-Platform/main.go
@@ -0,0 +1,66 @@
+package main
+
+
+import(
+"fmt"
+"math/rand"
+"time"
+"github.com/gin-gonic/gin"
+)
+
+
+
+type Quiz struct {
+	ID			string
+	Question	string
+	Options		[]string
+	Answer		string
+
+}
+
+
+var quizzes []Quiz
+func main() {
+	r:= gin.Default()
+
+	r.GET("/quiz",getRandomQuiz)
+	r.Run(":8080")
+
+}
+
+
+func getRandomQuiz(c *gin.Context) {
+if len quizzes == 0 {
+	c.JSON(400,gin.H{"error":"No quizzes available"})
+	return
+}
+
+rand.Seed(time.Now().UnixNano())
+randomIndex := rand.Intn(len(quizzes))
+quiz := quizzes[randomIndex]
+c.JSON(200,quiz)
+}
+
+
+func init() {
+	quizzes = append(quizzes,Quiz { 
+		ID:"1"
+		Question:"What is the Capital of france?",
+		Options:[]string{"Berlin", "Paris", "London", "Rome"},
+		Answer:   "Paris",
+	
+	
+})
+quizzes = append(quizzes, Quiz{
+	ID:       "2",
+	Question: "Which planet is known as the Red Planet?",
+	Options:  []string{"Earth", "Mars", "Jupiter", "Venus"},
+	Answer:   "Mars",
+})
+quizzes = append(quizzes, Quiz{
+	ID:       "3",
+	Question: "What is the largest mammal on Earth?",
+	Options:  []string{"Elephant", "Blue Whale", "Giraffe", "Hippopotamus"},
+	Answer:   "Blue Whale",
+})
+}
